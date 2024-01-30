da110b488d0911f3f81681e5ccb1f898a080c41d
Berozashvil1 <giorgiberozashvili2@gmail.com>
2024-01-30 17:59:01 +0400

    add

---
+++ 30.01.2024/30.01.js
@@ -0,0 +1,27 @@
+// let a=15 - number
+// let a='15 - string
+// let a=true - boolean
+// let a=false - boolean
+// let a=undefined
+// let a=null
+
+
+// let a=15;
+// let b=13;
+// let bol=a+b
+// console.log(bol)
+
+// let born=1987;
+// let now=2024;
+// const age=now-born
+// console.log(age)
+
+let fName='gela';
+let lName='gnolidze';
+let fullName=fName+' '+lName;
+console.log(fullName)
+
+fName='soso';
+let fullName: String;
+fullName=fName+''+lName;
+console.log(fullName)
