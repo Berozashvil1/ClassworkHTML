038eef2d1a3b22d5dd7b95ad357383daa03d9afb
Berozashvil1 <giorgiberozashvili2@gmail.com>
2024-02-07 18:08:11 +0400

    add

---
+++ 7.02.2024/loop.js
@@ -0,0 +1,51 @@
+
+// const arr=[1,2,3,4,5,6,7,8,9];
+
+// for(let i=0; i<arr.length; i++){
+//     // arr[i]+=10
+//     arr[i]=arr[i]+10
+// }
+
+// console.log(arr)
+
+// const loop=[1,2,3,4,5,6,7,8,9,10,11,12,13]
+// const loop2=[]
+// const loop3=[]
+
+// for(let i=0; i<loop.length; i=i*10){
+//     loop[i]*=10
+// }
+
+// for(let a=0; a<loop.length; a++){
+//     if(loop[a]%2===0){
+//         loop2.push[loop[a]]
+//     }
+// }
+
+// for(let a=0; a<loop.length; a++){
+//     if(loop[a]%2>0){
+//         loop3.push[loop[a]]
+//     }
+// }
+
+// console.log(loop)
+// console.log(loop2)
+// console.log(loop3)
+
+// const arr=[[1,2,3],[4,5,6],[7,8,9,10]]
+// console.log(arr)
+
+//     for(let i=0; i<arr.length; i++){
+//         for(let a=0; a<arr[i].length; a++){
+//             arr[i][a]+=10
+//         }
+//     }
+
+const arr=[1,2,3,4,5,6,7,8,9,10]
+console.log(arr)
+
+for(let i=0; 1<arr.length;){
+    if(arr[i]%2>0){
+        
+    }
+}
